f648ab2fd22a6d0af8afc6547d8ad581868a26eb
jtfrench <josh.fr@shaw.ca>
2020-10-14 21:43:40 -0600

    implemented 2/3 of authentication protocol

--- A3-VPN/client.py
+++ A3-VPN/client.py
@@ -1,12 +1,28 @@
 import socket
 import sys
+import random
+import os
+from Crypto.Cipher import AES
 
 HOST = '127.0.0.1'
 PORT = 65432
 
+K_AB = input("Hi TA, please enter the shared secret value: ")
+K_AB = K_AB.zfill(16) #pad the key to be 16 bytes
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    R_A = os.urandom(16) # generate R_A
+    print("Client generated a nonce (R_A): ", R_A)
+    s.sendall(R_A) #send R_A
+    print("Client sent nonce R_A.")
+    R_B = s.recv(16) # receive R_B
+    print("Client received a nonce (R_B): ", R_B)
+    message = s.recv(1024) # receive E("Bob",R_A,K_AB)
+    print("Client received a message: ", message)
+    #next step: extract IP ("Bob") and R_A from message by decrypting
+
+
     while True:
         data = s.recv(1024)
         print('Client Received: ' + data.decode("utf-8"))
--- A3-VPN/server.py
+++ A3-VPN/server.py
@@ -1,18 +1,42 @@
 import socket
 import sys
+import random
+import os
+from Crypto.Cipher import AES
 
 HOST = '127.0.0.1'
 PORT = 65432
+
+K_AB = input("Hi TA, please enter the shared secret value: ")
+K_AB = K_AB.zfill(16) #pad the key to be 16 bytes
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print('Connected by', addr)
+        print('Connected by', addr) #eq to "I'm Alice"
+        R_A = conn.recv(16) #receive R_A
+        print("Server received a nonce (R_A): ", R_A)
+        R_B = os.urandom(16) # generate R_B
+        print("Server generated a nonce (R_B): ", R_B)
+        conn.sendall(R_B) #send R_B
+        print("Server sent nonce R_B.")
+        message = HOST + str(R_A) #concantenate "Bob" and R_A
+        n = len(message)
+        if n % 16 != 0:
+            message += '0' * (16 - n % 16) #padded with spaces 
+        print("Generated plaintext message: ", message)
+        print('The length of the message is ', len(message))
+        aes = AES.new(K_AB, AES.MODE_CBC, R_B)
+        encd = aes.encrypt(message)
+        conn.sendall(encd) # send E("Bob",R_A,K_AB)
+        print("Server sent encypted message.")
+
         while True:
             data = conn.recv(1024)
-            print("Server received: " + data.decode("utf-8"))
+            print('Server Received: ' + data.decode("utf-8"))
             input_var = input("Reply: ")
             input_var = input_var.encode("utf-8")
             conn.sendall(input_var)
