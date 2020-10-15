5bd641b696a0efb35cc864a4024bd63641663fac
jtfrench <josh.fr@shaw.ca>
2020-10-15 10:20:39 -0600

    added final authentication step

--- A3-VPN/client.py
+++ A3-VPN/client.py
@@ -7,22 +7,43 @@ from Crypto.Cipher import AES
 HOST = '127.0.0.1'
 PORT = 65432
 
-K_AB = input("Hi TA, please enter the shared secret value: ")
+K_AB = '123' #input("Hi TA, please enter the shared secret value: ")
 K_AB = K_AB.zfill(16) #pad the key to be 16 bytes
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #1st arrow in Figure 9.12
     s.connect((HOST, PORT))
     R_A = os.urandom(16) # generate R_A
     print("Client generated a nonce (R_A): ", R_A)
     s.sendall(R_A) #send R_A
     print("Client sent nonce R_A.")
+    
+    #2nd arrow in Figure 9.12
     R_B = s.recv(16) # receive R_B
     print("Client received a nonce (R_B): ", R_B)
     message = s.recv(1024) # receive E("Bob",R_A,K_AB)
     print("Client received a message: ", message)
-    #next step: extract IP ("Bob") and R_A from message by decrypting
+    aes = AES.new(K_AB, AES.MODE_CBC, R_A) 
+    decd = aes.decrypt(message) #decrypt using K_AB and R_A
+    server_IP = str(decd.rstrip().decode())
+    if(server_IP == HOST):
+        print("Server IP address is a match.")
+        print("Authentication is: VALID")
+    else: 
+        print("Server IP address is NOT a match")
+        raise Exception("Authentication is: INVALID")
 
+    #3rd arrow in Figure 9.12
+    message = socket.gethostbyname(socket.gethostname()) # get own IP
+    n = len(message)
+    if n % 16 != 0:
+        message += ' ' * (16 - n % 16) #padded with spaces 
 
+    aes = AES.new(K_AB, AES.MODE_CBC, R_B)
+    encd = aes.encrypt(message) #encrypt "Alice" using R_B and K_AB
+    s.sendall(encd) #send E("Alice",R_B,K_AB)
+
+    #TASK: setup AES encryption for continual communication
     while True:
         data = s.recv(1024)
         print('Client Received: ' + data.decode("utf-8"))
--- A3-VPN/server.py
+++ A3-VPN/server.py
@@ -7,7 +7,7 @@ from Crypto.Cipher import AES
 HOST = '127.0.0.1'
 PORT = 65432
 
-K_AB = input("Hi TA, please enter the shared secret value: ")
+K_AB = '123' #input("Hi TA, please enter the shared secret value: ")
 K_AB = K_AB.zfill(16) #pad the key to be 16 bytes
 
 
@@ -16,24 +16,40 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, addr = s.accept()
     with conn:
+        #1st arrow in Figure 9.12
         print('Connected by', addr) #eq to "I'm Alice"
         R_A = conn.recv(16) #receive R_A
         print("Server received a nonce (R_A): ", R_A)
         R_B = os.urandom(16) # generate R_B
+        
+        #2nd arrow in Figure 9.12
         print("Server generated a nonce (R_B): ", R_B)
         conn.sendall(R_B) #send R_B
         print("Server sent nonce R_B.")
-        message = HOST + str(R_A) #concantenate "Bob" and R_A
+        message = HOST #eq to "Bob"
         n = len(message)
         if n % 16 != 0:
-            message += '0' * (16 - n % 16) #padded with spaces 
+            message += ' ' * (16 - n % 16) #padded with spaces 
         print("Generated plaintext message: ", message)
-        print('The length of the message is ', len(message))
-        aes = AES.new(K_AB, AES.MODE_CBC, R_B)
-        encd = aes.encrypt(message)
+        aes = AES.new(K_AB, AES.MODE_CBC, R_A)
+        encd = aes.encrypt(message) #encrypt "Bob" using R_A and K_AB
         conn.sendall(encd) # send E("Bob",R_A,K_AB)
         print("Server sent encypted message.")
 
+        #3rd arrow in Figure 9.12
+        message = conn.recv(1024)
+        aes = AES.new(K_AB, AES.MODE_CBC, R_B) 
+        decd = aes.decrypt(message) #decrypt using K_AB and R_A
+        client_IP = str(decd.rstrip().decode())
+        #this authentication will not work atm because program is being run on same machine
+        if(client_IP == addr):
+            print("Client IP address is a match.")
+            print("Authentication is: VALID")
+        else: 
+            print("Client IP address is NOT a match")
+            raise Exception("Authentication is: INVALID")
+
+        #TASK: setup AES encryption for continual communication
         while True:
             data = conn.recv(1024)
             print('Server Received: ' + data.decode("utf-8"))
