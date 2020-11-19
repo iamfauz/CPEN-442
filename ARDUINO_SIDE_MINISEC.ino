74da3661643174eeae1d4854fa6a7b9b0f98350e
maximilianjohnson <46610098+maximilianjohnson@users.noreply.github.com>
2020-11-18 17:17:33 -0800

    Add files via upload

---
+++ ARDUINO_SIDE_MINISEC.ino
@@ -0,0 +1,12 @@
+int data; //Initialized variable to store recieved data
+
+void setup() {
+  //Serial Begin at 9600 Baud 
+  Serial.begin(9600);
+}
+
+void loop() {
+  data = Serial.read(); //Read the serial data and store it
+  Serial.println(data);
+  delay(1000);
+}
---
+++ ESP_SIDE_MINISEC.ino
@@ -0,0 +1,13 @@
+#include<SoftwareSerial.h> //Included SoftwareSerial Library
+SoftwareSerial s(3,1);
+
+void setup() {
+  //Serial S Begin at 9600 Baud
+  s.begin(9600);
+}
+
+void loop() {
+  //Write '123' to Serial
+  s.write(123);
+  delay(1000);
+}
